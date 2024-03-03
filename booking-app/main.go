c4549214b88aa7449d0e159d0c7883bbb12c50a3
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-03-03 16:35:21 +0200

    Fix: move getName to the separate func

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,15 +1,30 @@
+// https://youtu.be/yyUHQIec83I?si=S-Ize7JqZ-mR-oxN&t=3761
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Welcome!")
+func getName() string {
 	fmt.Println("Print your name:")
 
 	var userName string
 	fmt.Scan(&userName)
 
 	fmt.Printf("Okay, your name is %v.", userName)
+
+	return userName
+}
+
+func main() {
+	fmt.Println("Welcome!")
+
+	//var userName string = getName()
+
+	const maxTickets uint8 = 50
+	var remainingTickets uint = 50
+
+	var bookings [50]string
+
+
 
 }
 
