778bbb1cc7efe3e5d9bda14e11cda0e107adc402
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-03-03 16:23:54 +0200

    Fix: added Scan for userName

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,5 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome!")
+	fmt.Println("Print your name:")
+
+	var userName string
+	fmt.Scan(&userName)
+
+	fmt.Printf("Okay, your name is %v.", userName)
+
 }
 
