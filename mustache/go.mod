cceeddf4b26aef8fa427bd7e4e3c5ae9a394a45d
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-03-04 16:52:51 +0200

    Feat: added test module for Mustache

---
+++ mustache/go.mod
@@ -0,0 +1,5 @@
+module mustache
+
+go 1.22.0
+
+require github.com/cbroglie/mustache v1.4.0 // indirect
---
+++ mustache/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cbroglie/mustache"
+)
+
+// Product struct definition
+type Product struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+
+func main() {
+	// Creating an instance of the Product struct
+	product := Product{
+		Name:    "Some product",
+		Address: "Some street",
+	}
+
+	// Convert struct to map
+	productMap := make(map[string]interface{})
+	productJSON, _ := json.Marshal(product)
+	json.Unmarshal(productJSON, &productMap)
+
+	// Template string
+	templateString := "Product Name: {{name}}, Address: {{address}}"
+
+	// Render the template
+	renderedString, err := mustache.Render(templateString, productMap)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Print the rendered template
+	fmt.Println(renderedString)
+}
