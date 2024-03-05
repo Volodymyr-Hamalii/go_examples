dd2ce4de063a8f68f004ee66d6e66bcf16924e96
volodymyr-hamalii <hamalii.volodymyr@gmail.com>
2024-03-05 16:40:21 +0200

    Feat: added nested items

--- mustache/main.go
+++ mustache/main.go
@@ -12,7 +12,7 @@ type Product struct {
 	Address string `json:"address"`
 }
 
-func main() {
+func renderTestStr() {
 	// Creating an instance of the Product struct
 	product := Product{
 		Name:    "Some product",
@@ -38,4 +38,43 @@ func main() {
 
 	// Print the rendered template
 	fmt.Println(renderedString)
+}
+
+func renderNestedItem() {
+	data := map[string]interface{}{
+		"itinerary": []map[string]interface{}{
+			{
+				"itinerary_items": []map[string]interface{}{
+					{
+						"location": map[string]interface{}{
+							"city": "City1",
+						},
+						"name": "Item1",
+					},
+					{
+						"location": map[string]interface{}{
+							"city": "City2",
+						},
+						"name": "Item2",
+					},
+				},
+			},
+		},
+	}
+
+	// Adjust the template to render the name of the first item
+	template := "{{#itinerary}}{{#itinerary_items}}{{#location}}{{#city}}{{name}}{{/city}}{{/location}}{{/itinerary_items}}{{/itinerary}}"
+
+
+	result, err := mustache.Render(template, data)
+	if err != nil {
+		fmt.Println("Error rendering template:", err)
+		return
+	}
+
+	fmt.Println(result)
+}
+
+func main() {
+	renderNestedItem()
 }
